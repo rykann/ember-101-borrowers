63dba670c8cc5596fe46fdb27b454b24b9216f90
Ryan Kannegiesser <rkannegiesser@gmail.com>
2015-12-10 17:39:42 -0600

    Through page 38

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
-
 export default Ember.Route.extend({
   actions: {
     save() {
-      console.log('+--- save action bubbled up to friends route');
+      console.log('+---- save action bubbled up to application route');
       return true;
     },
     cancel() {
-      console.log('+--- cancel action bubbled up to friends route');
+      console.log('+---- cancel action bubbled up to application route');
       return true;
     }
   }
