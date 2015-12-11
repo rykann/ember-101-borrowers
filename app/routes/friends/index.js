b0b625437c7510a6f8d4f8a697ab7a232802c7d7
Ryan Kannegiesser <rkannegiesser@gmail.com>
2015-12-10 18:02:49 -0600

    Implement friend deletion

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -4,5 +4,12 @@ export default Ember.Route.extend({
 
   model() {
     return this.store.findAll('friend');
+  },
+
+  actions: {
+    delete(friend) {
+      friend.destroyRecord();
+      return false;
+    }
   }
 });
