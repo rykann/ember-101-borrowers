33b3cb31821ec7049605a6650e9cbd1ba8a8b397
Ryan Kannegiesser <rkannegiesser@gmail.com>
2015-12-10 18:08:15 -0600

    Move delete action to parent route

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -1,15 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
   model() {
     return this.store.findAll('friend');
   },
-
-  actions: {
-    delete(friend) {
-      friend.destroyRecord();
-      return false;
-    }
-  }
 });
