2a95814a7ada40865acd4d28287823022b9c7427
Ryan Kannegiesser <rkannegiesser@gmail.com>
2015-12-11 16:54:54 -0600

    Add articles/new controller

---
+++ app/controllers/articles/new.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.description',
+    {
+      get() {
+        return !Ember.isEmpty(this.get('model.description'))
+      }
+    }
+  ),
+  actions: {
+    save() {
+      if (!this.get('isValid')) {
+        this.set('errorMessage', 'You have to fill all the fields');
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+});
