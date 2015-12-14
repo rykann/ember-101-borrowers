9c012d5e71ee14d5afca420bff203324cb216dd4
Ryan Kannegiesser <rkannegiesser@gmail.com>
2015-12-14 17:16:43 -0600

    Add article-row component
    * Displays state of an article
    * Has a drop-down for choosing article state
    * Has a save button

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -4,5 +4,11 @@ export default Ember.Route.extend({
   model: function() {
     console.log("articles index model hook: " + this.modelFor('friends/show').get('articles'))
     return this.modelFor('friends/show').get('articles')
+  },
+  actions: {
+    save(model) {
+      model.save();
+      return false;
+    }
   }
 });
