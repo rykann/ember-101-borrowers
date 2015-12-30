84f6f3febb6cf712add938370c4d049bafff4773
Ryan Kannegiesser <rkannegiesser@gmail.com>
2015-12-30 11:32:03 -0600

    Add missing semicolons

--- app/helpers/formatted-date.js
+++ app/helpers/formatted-date.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import { formatDate } from '../utils/date-helpers';
 
 export function formattedDate([date, format]) {
-  return formatDate(date, format)
+  return formatDate(date, format);
 }
 
 export default Ember.Helper.helper(formattedDate);
